24d9ea30454ec57b1d398a0ece39bcab1ea7afa2
shivam <shivamyadav343343@gmail.com>
2024-03-13 09:44:07 +0530

    commiting

---
+++ src/atmmanagement/AtmManagement.java
@@ -0,0 +1,14 @@
+
+
+package atmmanagement;
+
+public class AtmManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
